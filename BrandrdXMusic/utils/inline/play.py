8c45c3a50fd992c5447e987f12b594b40a8d8423
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-08-14 11:08:33 +0530

    Update play.py

--- BrandrdXMusic/utils/inline/play.py
+++ BrandrdXMusic/utils/inline/play.py
@@ -68,10 +68,10 @@ def stream_markup_timer(_, vidid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/ll_SARKAR_MERA_BABU_ll"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/AllllllllllllllIllllllll"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/TKS_CHAT_JOIN_BABY"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/Ak_support1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -90,10 +90,10 @@ def stream_markup(_, videoid, chat_id):
         ],
         [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/ll_SARKAR_MERA_BABU_ll"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/AllllllllllllllIllllllll"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/TKS_CHAT_JOIN_BABY"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/Ak_support1"
             ),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -245,10 +245,10 @@ def stream_markup2(_, chat_id):
         ],
 [
             InlineKeyboardButton(
-                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/ll_SARKAR_MERA_BABU_ll"
+                text="⛦ ᴏᴡɴᴇʀ ❥", url=f"https://t.me/AllllllllllllllIllllllll"
             ),
             InlineKeyboardButton(
-                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/TG_NAME_STYLE"
+                text="⛦ ᴄʜᴀᴛ ❥", url=f"https://t.me/Ak_support1"
             ),
         ],
 

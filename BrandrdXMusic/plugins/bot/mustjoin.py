1d74af19ecf726ad42645951bcf4bbb98f92874a
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-08-15 14:40:17 +0530

    Update mustjoin.py

--- BrandrdXMusic/plugins/bot/mustjoin.py
+++ BrandrdXMusic/plugins/bot/mustjoin.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
-from ANNIEMUSIC import app
+from BrandrdXMusic import app
 
 #--------------------------
 

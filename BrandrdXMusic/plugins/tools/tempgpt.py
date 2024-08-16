d8533ab52b310da100cfb13ca0375e3218085f5c
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-08-16 15:28:04 +0530

    Update tempgpt.py

--- BrandrdXMusic/plugins/tools/tempgpt.py
+++ BrandrdXMusic/plugins/tools/tempgpt.py
@@ -5,7 +5,7 @@ from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 from MukeshAPI import api
 
-@app.on_message(filters.command(["chatgpt", "ai", "ask", "arvis", "iri"], prefixes=[".", "J", "j", "s", "S", "/"]))
+@app.on_message(filters.command(["chatgpt", "ai", "ask", "", "iri"], prefixes=[".", "J", "j", "s", "", "/"]))
 async def chat_gpt(bot, message):
     try:
         await bot.send_chat_action(message.chat.id, ChatAction.TYPING)

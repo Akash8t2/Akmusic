149804ed810bae638f09c2b952f3cbd2e6962379
Akash <158745842+Akash8t2@users.noreply.github.com>
2024-11-25 23:16:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,6 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 
 # Chat id of a group for logging bot's activities
 LOGGER_ID = int(getenv("LOGGER_ID", None))
-LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1002120694279"))
 
 # Get this value from @STRING_SESSIONS_Bot on Telegram by /id
 OWNER_ID = int(getenv("OWNER_ID"))
